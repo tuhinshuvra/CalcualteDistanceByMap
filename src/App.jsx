8f40fa858db284136b933931f7b074ea388c9536
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2024-01-29 17:23:32 +0600

    show storelist and there location in open map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import UserGEOLocationSetForm from "./Components/UserGEOLocationSetForm";
+import './App.css';
+import StoreList from './Components/StoreList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className=" container">
-      <UserGEOLocationSetForm></UserGEOLocationSetForm>
+    <div>
+      <StoreList />
     </div>
   );
 }
---
+++ src/Components/StoreList.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import storeData from '../data/storeData.json';
+import L from 'leaflet';
+import './StoreList.css'; // Assuming you have this CSS file imported
+
+const StoreList = () => {
+    const [map, setMap] = useState(null);
+
+    useEffect(() => {
+        const myMap = L.map('map').setView([23.7984463, 90.4031033], 7);
+        const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by coder\'s gyan with ❤️';
+        const tileLayer = L.tileLayer(tileUrl, { attribution });
+        tileLayer.addTo(myMap);
+        setMap(myMap);
+
+        // Create custom icon
+        const myIcon = L.icon({
+            iconUrl: 'sadhin.png',
+            iconSize: [30, 40]
+        });
+
+        // Add GeoJSON layer with custom icon and onEachFeature function
+        const shopsLayer = L.geoJSON(storeData, {
+            onEachFeature: onEachFeature,
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: myIcon });
+            }
+        });
+        shopsLayer.addTo(myMap);
+
+        return () => {
+            myMap.remove();
+        };
+    }, [storeData]); // Adding storeList to dependencies as it might change
+
+    const onEachFeature = (feature, layer) => {
+        // Define what happens on click of each feature
+        layer.bindPopup(makePopupContent(feature), { closeButton: false, offset: L.point(0, -8) });
+    };
+
+    const makePopupContent = (shop) => {
+        return (
+            <div>
+                <h4>{shop.properties.name}</h4>
+                <p>{shop.properties.address}</p>
+                <div className="phone-number">
+                    <a href={`tel:${shop.properties.phone}`}>{shop.properties.phone}</a>
+                </div>
+            </div>
+        );
+    };
+
+    const flyToStore = (store) => {
+        const lat = store.geometry.coordinates[1];
+        const lng = store.geometry.coordinates[0];
+        map.flyTo([lat, lng], 14, { duration: 3 });
+        setTimeout(() => {
+            L.popup({ closeButton: false, offset: L.point(0, -8) })
+                .setLatLng([lat, lng])
+                .setContent(makePopupContent(store))
+                .openOn(map);
+        }, 3000);
+    };
+
+    const generateList = () => {
+        return storeData.map((shop, index) => (
+            <li key={index}>
+                <div className="shop-item">
+                    <a href="#" onClick={() => flyToStore(shop)}>{shop.properties.name}</a>
+                    <p>{shop.properties.address}</p>
+                </div>
+            </li>
+        ));
+    };
+
+    return (
+        <div>
+            <main className='col-12 d-flex'>
+                <div className="store-list col-3">
+                    <div className="heading">
+                        <h2>Our outlets</h2>
+                    </div>
+                    <ul className="list">
+                        {generateList()}
+                    </ul>
+                </div>
+                <div className='col-9' id="map" />
+            </main>
+        </div>
+    );
+};
+
+export default StoreList;
