11bfb59053a5fa7b11db734dc6190f08b55bcf34
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2024-02-18 11:29:27 +0600

    remove unnecessary code

--- src/Components/TransmissionModule/ExistingConnectionStatus.jsx
+++ src/Components/TransmissionModule/ExistingConnectionStatus.jsx
@@ -3,13 +3,11 @@ import L from 'leaflet';
 import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import SadhinIcon from '../../assets/sadhin.png';
-import './ExistingConnectionStatus.css';
 import configUrl from '../../api/config';
+import './ExistingConnectionStatus.css';
 
 const ExistingConnectionStatus = () => {
     const [storeData, setStoredData] = useState(null);
-    const [map, setMap] = useState(null);
-    // const [searchData, setSearchData] = useState(null);
     const [searchData, setSearchData] = useState({
         division: '',
         district: '',
@@ -35,12 +33,9 @@ const ExistingConnectionStatus = () => {
             try {
                 const response = await fetch(`${configUrl.BASEURL}/api/v1/wificonnection`);
                 const data = await response.json();
-                // console.log("All Saved Connections: ", data.status);
-
                 if (data.status === 'success') {
                     setStoredData(data.data);
                 }
-
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -56,7 +51,6 @@ const ExistingConnectionStatus = () => {
             [name]: value
         }));
     };
-
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -84,8 +78,6 @@ const ExistingConnectionStatus = () => {
 
         const tileLayer = L.tileLayer(tileUrl, { attribution });
         tileLayer.addTo(myMap);
-
-        setMap(myMap);
 
         if (searchData) {
             const { division, district, thana, union, village, holding, direction, address } = searchData;
@@ -141,7 +133,6 @@ const ExistingConnectionStatus = () => {
 
         // Add GeoJSON layer with custom icon and onEachFeature function
         const shopsLayer = L.geoJSON(storeData, {
-            onEachFeature: onEachFeature,
             pointToLayer: function (feature, latlng) {
                 return L.marker(latlng, { icon: myIcon });
             }
@@ -154,8 +145,6 @@ const ExistingConnectionStatus = () => {
         };
     }, [searchData, storeData]);
 
-    // console.log("searchLocationLatLng ===> ", searchLocationLatLng);
-
 
     useEffect(() => {
         let totalCost = 0;
@@ -164,54 +153,6 @@ const ExistingConnectionStatus = () => {
         totalCost = setupCost + estimatedDistance * 15;
         setEstimatedCost(totalCost.toFixed(2));
     }, [estimatedDistance]);
-
-    const onEachFeature = (feature, layer) => {
-        // Define what happens on click of each feature
-        layer.bindPopup(makePopupContent(feature), { closeButton: false, offset: L.point(0, -8) });
-    };
-
-    const makePopupContent = (shop) => {
-        return (
-            <div>
-                <h4>{shop.name}</h4>
-                <p>{shop.locationName}</p>
-                <div className="phone-number">
-                    <a href={`tel:${shop.phone}`}>{shop.phone}</a>
-                </div>
-            </div>
-        );
-    };
-
-    const flyToStore = (store) => {
-        const lat = store.coordinates[1];
-        const lng = store.coordinates[0];
-        map.flyTo([lat, lng], 15, { duration: 3 });
-        setTimeout(() => {
-            L.popup({ closeButton: false, offset: L.point(0, -8) })
-                .setLatLng([lat, lng])
-                .setContent(makePopupContent(store))
-                .openOn(map);
-        }, 3000);
-
-    };
-
-    // generateExistingLocationList
-    const generateList = () => {
-        if (!storeData || storeData.length === 0) {
-            return <li>No store data available.</li>;
-        }
-
-        return storeData.map((shop, index) => (
-            <li key={index}>
-                <div className="shop-item">
-                    <a href="#" onClick={() => flyToStore(shop)}>{shop.name}</a>
-                    <p>{shop.locationName}</p>
-                </div>
-            </li>
-        ));
-    };
-
-
 
     // Function to find the nearest store
     const findNearestStore = (searchLocation) => {
@@ -229,9 +170,7 @@ const ExistingConnectionStatus = () => {
             for (let i = 0; i < storeData.length; i++) {
                 const store = storeData[i];
 
-                // console.log("storeLocation ==>", store);
                 const storeLocation = L.latLng(store.coordinates[1], store.coordinates[0]);
-
                 const distance = searchLocation.distanceTo(storeLocation);
                 if (distance < shortestDistance) {
                     shortestDistance = distance;
@@ -259,11 +198,7 @@ const ExistingConnectionStatus = () => {
                                     id="division"
                                     name="division"
                                     aria-describedby="division"
-                                    // value={division}
-                                    // onChange={(e) => setDivision(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, division: e.target.value })}
                                     onChange={handleInputChange}
-
                                 />
                             </div>
                             <div className='col'>
@@ -274,9 +209,6 @@ const ExistingConnectionStatus = () => {
                                     id="district"
                                     name="district"
                                     aria-describedby="district"
-                                    // value={district}
-                                    // onChange={(e) => setDistrict(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, district: e.target.value })}
                                     onChange={handleInputChange}
                                 />
                             </div>
@@ -291,9 +223,6 @@ const ExistingConnectionStatus = () => {
                                     id="thana"
                                     name="thana"
                                     aria-describedby="thana"
-                                    // value={thana}
-                                    // onChange={(e) => setThana(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, thana: e.target.value })}
                                     onChange={handleInputChange}
                                 />
                             </div>
@@ -305,9 +234,6 @@ const ExistingConnectionStatus = () => {
                                     id="union"
                                     name="union"
                                     aria-describedby="union"
-                                    // value={union}
-                                    // onChange={(e) => setUnion(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, union: e.target.value })}
                                     onChange={handleInputChange}
                                 />
                             </div>
@@ -324,9 +250,6 @@ const ExistingConnectionStatus = () => {
                                     id="village"
                                     name="village"
                                     aria-describedby="village"
-                                    // value={village}
-                                    // onChange={(e) => setVillage(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, village: e.target.value })}
                                     onChange={handleInputChange}
                                 />
                             </div>
@@ -338,9 +261,6 @@ const ExistingConnectionStatus = () => {
                                     id="holding"
                                     name="holding"
                                     aria-describedby="holding"
-                                    // value={holding}
-                                    // onChange={(e) => setHolding(e.target.value)}
-                                    // onChange={(e) => setSearchData({ ...searchData, holding: e.target.value })}
                                     onChange={handleInputChange}
                                 />
                             </div>
@@ -355,9 +275,6 @@ const ExistingConnectionStatus = () => {
                                 id="direction"
                                 name="direction"
                                 aria-describedby="direction"
-                                // value={direction}
-                                // onChange={(e) => setDirection(e.target.value)}
-                                // onChange={(e) => setSearchData({ ...searchData, direction: e.target.value })}
                                 onChange={handleInputChange}
                             />
                         </div>
@@ -370,24 +287,20 @@ const ExistingConnectionStatus = () => {
                                 id="address"
                                 name="address"
                                 aria-describedby="address"
-                                // value={address}
-                                // onChange={(e) => setAddress(e.target.value)}
-                                // onChange={(e) => setSearchData({ ...searchData, address: e.target.value })}
                                 onChange={handleInputChange}
                             />
                         </div>
 
-
                         <button type="submit" className="btn btn-primary">
                             Search
                         </button>
+
                     </form>
 
                     {
                         estimatedDistance &&
                         <div className=' my-4 fw-bold'>
                             <p> <b>Selected Position :</b> {newPos?.lat.toFixed(6)},{newPos?.lng.toFixed(6)} </p>
-
                             <p className=' fw-bold text-primary mb-0'> <b>Estimated Cost</b></p>
                             <p className=' mb-0'>Distance from nearest point = {estimatedDistance} Meter </p>
                             <p className=' mt-0'>Total Cost(SetupCost + Cable ) = {estimatedCost} TK</p>
@@ -396,15 +309,6 @@ const ExistingConnectionStatus = () => {
                 </div>
             </div>
             <main className='col-9'>
-                {/* <div className="store-list col-2"> */}
-                <div className="store-list">
-                    <div className="heading">
-                        <h3>Connections</h3>
-                    </div>
-                    <ul className="list">
-                        {generateList()}
-                    </ul>
-                </div>
                 <div className='col-12' id="map" />
             </main>
         </div>
